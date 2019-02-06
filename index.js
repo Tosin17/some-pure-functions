023af93d251846a029c042973e8922db0d617125
kajopelayeola@gmail <okajopel@cisco.com>
2019-02-05 21:21:07 -0500

    Clean up comments

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 
+// To reduce typing
 const l = console.log;
 
 const str = 'C is for cookie, that\'s good enough for me';
 
 // Input :String --> 'C is for cookie, that\'s good enough for me'
 // Output :String --> 'C 15 for cook13, 7h47'5 good 3nough for m3​​​​​'
-function leet(str) {
 
+function leet(str) {
     const leetObjMapping = {
         'a': 4,
         'A': 4,
@@ -30,6 +31,7 @@ function leet(str) {
     // For each xter in string, if it exists in the `letObjMapping` 
     // then store its map value in a new string, else just concatenante
     // it to the newString
+
     for (let c of str) {
         newStr += (leetObjMapping[c]) ? leetObjMapping[c] : c;
     }
@@ -43,11 +45,13 @@ l(leet(str));
 
 // Input --> 'hhhhhqqlllllllhhhppp'
 // Output --> 'h5q2l7h3p3,'
+
 function summarizeSerially(str) {
     const arr = [];
 
     // Compose input into ['hhhhh', 'qq', 'lllllll', 'hhhppp']
     // And store in `lettersArr` 
+
     str.split('').reduce((acc, val) => {
         if (acc === val) {
             arr.push(acc);
@@ -60,6 +64,7 @@ function summarizeSerially(str) {
     const lettersArr = arr.join('').split(',');
     
     // Summarize ['hhhhh', 'qq', 'lllllll', 'hhhppp'] into h5q2l7h3p3
+    
     let summary = '';
     for (letters of lettersArr) {
         if (letters.length !== 0) {
